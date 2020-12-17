4a247d7836400485c215d1a7e764f3b5f07e8105
Su Kang <sukang1999@users.noreply.github.com>
2020-12-18 03:20:39 +0800

    initdata

--- src/schema.sql
+++ src/schema.sql
@@ -38,40 +38,76 @@ create table Manager (
 	deleted boolean not null
 );
 insert into Manager (username, password, truename, email, deleted) values (
-	'admin',
-	'123456',
-	'adminname',
-	'admin@123.com',
-	0
+	'manager1',
+	'manager1',
+	'SK',
+	'manager1@qq.com',
+	false
 );
 insert into Manager(username , password , truename, email,deleted)values(
-	'admin2',
-	'123456',
-	'adminname2',
-	'admin2@123.com',
-	0
+	'manager2',
+	'manager2',
+	'CSQ',
+	'manager2@qq.com',
+	false
+);
+insert into Manager (username, password, truename, email, deleted) values (
+	'manager3',
+	'manager3',
+	'LST',
+	'manager3@qq.com',
+	false
+);
+insert into Manager(username , password , truename, email,deleted)values(
+	'manager4',
+	'manager4',
+	'WSH',
+	'manager4@qq.com',
+	false
+);
+insert into Manager (username, password, truename, email, deleted) values (
+	'manager5',
+	'manager5',
+	'XWJ',
+	'manager5@qq.com',
+	false
+);
+insert into Manager(username , password , truename, email,deleted)values(
+	'manager6',
+	'manager6',
+	'LWK',
+	'manager6@qq.com',
+	false
 );
 insert into Poster (username, password, truename, email, locked, deleted) values (
 	'user1',
 	'user1',
-	'xiaoming1',
-	'xiaoming@qq.com',
+	'Zhang San',
+	'zhangsan@qq.com',
 	false,
 	false
 );
 insert into Poster (username, password, truename, email, locked, deleted) values (
 	'user2',
 	'user2',
-	'xiaoming2',
-	'xiaoming@qq.com',
+	'Li Si',
+	'lisi@qq.com',
+	false,
+	false
+);
+insert into Poster (username, password, truename, email, locked, deleted) values (
+	'user3',
+	'user3',
+	'Wang Wu',
+	'wangwu@qq.com',
 	false,
 	false
 );
 insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
 	1,
-	'homework1',
-	'123456789123456789',
-	'2020-12-17 02:00:00',
+	'Post1',
+	'大家加油！',
+	'2020-12-17 01:00:00',
 	0,
 	0,
 	false,
@@ -79,19 +115,99 @@ insert into Post (poster, postname, message, postedTime, follow, click, topped, 
 );
 insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
 	2,
-	'homework2',
-	'123456789123456789',
+	'Post2',
+	'努力努力！',
+	'2020-12-17 02:00:00',
+	0,
+	0,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	3,
+	'Post3',
+	'冲冲冲！',
 	'2020-12-17 03:00:00',
-	1,
-	1,
-	true,
+	0,
+	0,
+	false,
 	false
 );
 insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
 	1,
-	'homework3',
-	'123456789123456789',
+	'Post4',
+	'大家加油！',
 	'2020-12-17 04:00:00',
+	0,
+	0,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	2,
+	'Post5',
+	'努力努力！',
+	'2020-12-17 05:00:00',
+	0,
+	0,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	3,
+	'Post6',
+	'冲冲冲！',
+	'2020-12-17 06:00:00',
+	0,
+	0,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	1,
+	'Post7',
+	'大家加油！',
+	'2020-12-17 07:00:00',
+	0,
+	0,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	2,
+	'Post8',
+	'努力努力！',
+	'2020-12-17 08:00:00',
+	0,
+	0,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	3,
+	'Post9',
+	'冲冲冲！',
+	'2020-12-17 09:00:00',
+	0,
+	0,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	1,
+	'Post10',
+	'努力努力！',
+	'2020-12-17 10:00:00',
+	6,
+	6,
+	false,
+	false
+);
+insert into Post (poster, postname, message, postedTime, follow, click, topped, deleted) values (
+	2,
+	'Post11',
+	'冲冲冲！',
+	'2020-12-17 11:00:00',
 	0,
 	0,
 	false,
@@ -99,8 +215,43 @@ insert into Post (poster, postname, message, postedTime, follow, click, topped, 
 );
 insert into Reply (poster, postId, message, postedTime, deleted) values (
 	1,
-	select id from Post where postname='homework2',
+	10,
 	'666',
-	'2020-12-17 03:10:00',
+	'2020-12-17 10:10:00',
+	false
+);
+insert into Reply (poster, postId, message, postedTime, deleted) values (
+	2,
+	10,
+	'加油',
+	'2020-12-17 10:11:00',
+	false
+);
+insert into Reply (poster, postId, message, postedTime, deleted) values (
+	3,
+	10,
+	'冲',
+	'2020-12-17 10:13:00',
+	false
+);
+insert into Reply (poster, postId, message, postedTime, deleted) values (
+	1,
+	10,
+	'666',
+	'2020-12-17 10:20:20',
+	false
+);
+insert into Reply (poster, postId, message, postedTime, deleted) values (
+	2,
+	10,
+	'努力',
+	'2020-12-17 10:32:45',
+	false
+);
+insert into Reply (poster, postId, message, postedTime, deleted) values (
+	3,
+	10,
+	'Go',
+	'2020-12-17 10:56:07',
 	false
 );
